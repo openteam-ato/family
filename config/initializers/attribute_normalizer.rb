4a1a08dcd53e0daef8414c3f132d158b1bb72140
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-01-23 13:45:14 +0700

    Multiple value for living arrangement

--- config/initializers/attribute_normalizer.rb
+++ config/initializers/attribute_normalizer.rb
@@ -2,5 +2,9 @@ AttributeNormalizer.configure do |config|
   config.normalizers[:strip_array] = ->(value, options) do
     value.split(',').map(&:strip)
   end
+
+  config.normalizers[:blank_array] = ->(value, options) do
+    (value || []).delete_if(&:blank?)
+  end
 end
 
