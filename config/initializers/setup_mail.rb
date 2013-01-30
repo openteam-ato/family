42821de9c7eafbe1c3e2db4f43b6bb6c1dcd0d44
tab <tukhfatullin@gmail.com>
2013-01-30 11:55:08 +0700

    Fix settings for smtp server auth

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,9 @@
 domain   = Settings['mail']['domain']
-login    = Settings['mail']['login']
-password = Settings['mail']['password']
 smtp     = Settings['mail']['smtp']
 port     = Settings['mail']['port']
 
 ActionMailer::Base.smtp_settings = {
   :address              => smtp,
   :port                 => port,
-  :domain               => domain,
-  :user_name            => login,
-  :password             => password,
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :domain               => domain
 }
