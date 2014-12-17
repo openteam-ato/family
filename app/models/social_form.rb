502c78ee4514445b2976d54f7149ecf58649c852
Ivan Puchkov <rake.eclipse@gmail.com>
2014-12-17 11:53:27 +0600

    add enumerize fields to social_form and social_service

--- app/models/social_form.rb
+++ app/models/social_form.rb
@@ -1,6 +1,10 @@
 class SocialForm < ActiveRecord::Base
-  belongs_to :social_provider
+  extend Enumerize
+
   attr_accessible :free_places_number, :kind, :places_number
+  belongs_to :social_provider
+
+  enumerize :kind, :in => [:home, :semi_stationary, :stationary]
 
   validates_presence_of :free_places_number, :kind, :places_number
 end
--- app/models/social_service.rb
+++ app/models/social_service.rb
@@ -1,6 +1,10 @@
 class SocialService < ActiveRecord::Base
-  belongs_to :social_provider
+  extend Enumerize
+
   attr_accessible :kind, :price, :title
+  belongs_to :social_provider
+
+  enumerize :kind, :in => [:common, :medical, :psychological, :pedagogical, :labor, :legal, :communicative, :urgent]
 
   validates_presence_of :kind, :price, :title
 end
