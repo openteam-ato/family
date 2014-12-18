7284fa622cdd3dc6ae9c7eacd599bbd61012da95
Ivan Puchkov <rake.eclipse@gmail.com>
2014-12-18 11:09:28 +0600

    use authorization in social_provider controller

--- app/controllers/my/social_providers_controller.rb
+++ app/controllers/my/social_providers_controller.rb
@@ -1,3 +1,61 @@
 class My::SocialProvidersController < ApplicationController
-  inherit_resources
+  include Pundit
+
+  before_filter :user_not_authorized, :unless => 'current_user.present?'
+
+  def index
+    @social_providers = policy_scope(SocialProvider)
+  end
+
+
+  def show
+    @social_provider = SocialProvider.find(params[:id])
+    authorize @social_provider, :manage?
+  end
+
+  def new
+    @social_provider = current_user.social_providers.new
+    authorize @social_provider, :manage?
+  end
+
+  def create
+    @social_provider = current_user.social_providers.new(params[:social_provider])
+    authorize @social_provider, :manage?
+
+    if @social_provider.save!
+      redirect_to my_social_provider_path(@social_provider)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @social_provider = SocialProvider.find(params[:id])
+    authorize @social_provider, :manage?
+  end
+
+  def update
+    @social_provider = SocialProvider.find(params[:id])
+    authorize @social_provider, :manage?
+    if @social_provider.update_attributes(params[:social_provider])
+      redirect_to my_social_provider_path(@social_provider)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @social_provider = SocialProvider.find(params[:id])
+    authorize @social_provider, :manage?
+
+    @social_provider.destroy
+    redirect_to my_social_providers_path
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Необходимо произвести вход в систему"
+    redirect_to social_providers_path
+  end
 end
