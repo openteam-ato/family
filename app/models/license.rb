83385e9e313f2569c9a0fc284ab3f9ec0bd40e47
Evgeny Lapin <lev@openteam.ru>
2015-02-03 15:30:36 +0600

    do not validate expiration license date

--- app/models/license.rb
+++ app/models/license.rb
@@ -2,7 +2,7 @@ class License < ActiveRecord::Base
   belongs_to :social_provider, :touch => true
   attr_accessible :activity_kind, :expiration_date, :issue_date, :issuing_organization, :number, :seria
 
-  validates_presence_of :activity_kind, :expiration_date, :issue_date, :issuing_organization, :number, :seria
+  validates_presence_of :activity_kind, :issue_date, :issuing_organization, :number, :seria
 end
 
 # == Schema Information
